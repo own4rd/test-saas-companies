3ef21da44b9a1226d88e9d77c5448b5cca2bd5fa
Wagner <wagnerpalmeira.ti@gmail.com>
2024-02-18 14:38:16 -0300

    Criando migrations e modelo

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # THIRD APPS
+    "drf_yasg",
+    "rest_framework",
+    "rest_framework_simplejwt",
+    # PROJECT APPS
+    "users",
 ]
 
 MIDDLEWARE = [
